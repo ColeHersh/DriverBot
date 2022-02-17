38175080597cac9e5048d003357d460f72b3f401
Cole <cole900@att.net>
2022-02-16 18:57:34 -0800

    Pnuematics

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -35,7 +35,7 @@ import com.ctre.phoenix.led.ColorFlowAnimation.Direction;
  */
 public class Robot extends TimedRobot {
   
-  private final WPI_TalonSRX R1 = new WPI_TalonSRX(1);
+  /*private final WPI_TalonSRX R1 = new WPI_TalonSRX(1);
   private final WPI_TalonSRX R2 = new WPI_TalonSRX(2);
   
   MotorControllerGroup right = new MotorControllerGroup(R1, R2);
@@ -45,21 +45,23 @@ public class Robot extends TimedRobot {
 
   MotorControllerGroup left = new MotorControllerGroup(L1, L2);
 
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(left, right);
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(left, right);*/
   private final Joystick stick = new Joystick(1);
   private final Joystick stick2 = new Joystick(2);
 
-  private final PneumaticHub p = new PneumaticHub(63);
+  private final PneumaticHub p = new PneumaticHub(62);
   private final DoubleSolenoid d = p.makeDoubleSolenoid (8,9);
-  private final Compressor compress = p.makeCompressor();
+
+  private DoubleSolenoid.Value state = DoubleSolenoid.Value.kReverse;
+  //private final Compressor compress = new Compressor(PneumaticsModuleType.REVPH);
   //private final Compressor phCompressor = new Compressor(1, PneumaticsModuleType.REVPH);
 
-  private final CANdle candle = new CANdle (0);
+  /*private final CANdle candle = new CANdle (0);
   private final RainbowAnimation r = new RainbowAnimation(1,1,6);
   private final FireAnimation f = new FireAnimation(1,1,8,.2,.4);
   private final RgbFadeAnimation fa = new RgbFadeAnimation (1,.7,8);
   private final StrobeAnimation s = new StrobeAnimation (100,200,8, 255, 1, 8);
-  private final ColorFlowAnimation c = new ColorFlowAnimation(100,200,8, 255, 1, 8, Direction.Backward);
+  private final ColorFlowAnimation c = new ColorFlowAnimation(100,200,8, 255, 1, 8, Direction.Backward);*/
   Timer m_timer = new Timer();
   @Override
   public void robotInit() {
@@ -67,8 +69,10 @@ public class Robot extends TimedRobot {
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
     //m_rightMotor.setInverted(true);
-    right.setInverted(true);
-    candle.setLEDs (0,250,0);
+  
+  
+    //  right.setInverted(true); NOOOOOOOOOOOOOO
+   // candle.setLEDs (0,250,0);
   }
 
   
@@ -76,7 +80,7 @@ public class Robot extends TimedRobot {
   public void autonomousInit() {
       // TODO Auto-generated method stub
       super.autonomousInit();
-      candle.setLEDs (0,0,0);
+     // candle.setLEDs (0,0,0);
       m_timer.reset();
       m_timer.start();
   }
@@ -93,7 +97,7 @@ public class Robot extends TimedRobot {
          }
          else{
          // candle.setLEDs (0,255,255);*/
-          candle.animate(c);
+         // candle.animate(c);
          }
      
   @Override
@@ -101,19 +105,33 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    compress.enableAnalog(60,80);
-    if(stick2.getTrigger()){
-      candle.setLEDs (255,204,0);
-      d.toggle();
+    //compress.enableDigital();
+    
+    //compress.enableDigital();
+   
+    // p.reserveCompressor();
+    if(stick2.getTriggerPressed()){
+     // candle.setLEDs (255,204,0);
+     if(state == DoubleSolenoid.Value.kReverse){
+      state = DoubleSolenoid.Value.kForward;
+      d.set(state);
+     }
+     else{
+      state = DoubleSolenoid.Value.kReverse;
+      d.set(state);
+     }
+      //d.toggle();
     }
-    else if(stick.getTrigger()){
-      candle.setLEDs (255,0,0);
+    else if(stick.getTriggerPressed()){
+      //candle.setLEDs (255,0,0);
+     // d.set(DoubleSolenoid.Value.kReverse);
     }
-    else{candle.setLEDs (100,100,200);}
-  //  m_robotDrive.arcadeDrive(stick2.getZ(), -stick.getY());
+    else{//candle.setLEDs (100,100,200);
+    }
+   // m_robotDrive.arcadeDrive(stick2.getZ(), -stick.getY());
 
   
 
-     m_robotDrive.tankDrive(-stick2.getY(), stick.getY());
+     //m_robotDrive.tankDrive(-stick2.getY(), stick.getY());
   }
 }
